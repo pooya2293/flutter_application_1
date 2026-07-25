e4e0c79e2d3c1304329ccc447cccc9e008b301fc
Pouriya <poorya2293@gmail.com>
2026-07-25 22:45:30 +0330

    AuthField  form key make

--- lib/features/auth/presentaion/widgets/auth_field.dart
+++ lib/features/auth/presentaion/widgets/auth_field.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 class AuthField extends StatelessWidget {
   final String hintText;
+  final TextEditingController controller;
   const AuthField({
     super.key,
-    required this.hintText
+    required this.hintText,
+    required this.controller,
   });
 
   @override
@@ -13,6 +15,12 @@ class AuthField extends StatelessWidget {
       decoration: InputDecoration(
         hintText: hintText,
       ),
+      validator: (value){
+        if(value!.isEmpty){
+          return '$hintText is missing';
+        }
+        return null;
+      },
     );
   }
 }
